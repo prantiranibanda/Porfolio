341570f9eefb1b50d718ae786a0d085961d9e6f6
pranti <prantibanda@gmail.com>
2024-11-03 11:58:08 +0530

    links added

--- src/app/components/ContactIcon.jsx
+++ src/app/components/ContactIcon.jsx
@@ -1,13 +1,24 @@
+import Link from "next/link";
 import React from "react";
 
 const ContactIcon = () => {
 	return (
 		<div className="space-x-8 pt-8 text-white">
-			<i className="fi fi-brands-github inline-block text-2xl text-red-300 hover:animate-bounce hover:cursor-pointer hover:text-[#ff014f]"></i>
-			<i className="fi fi-brands-linkedin inline-block text-2xl text-red-300 hover:animate-bounce hover:cursor-pointer hover:text-[#ff014f]"></i>
-			<i className="fi fi-brands-instagram inline-block text-2xl text-red-300 hover:animate-bounce hover:cursor-pointer hover:text-[#ff014f]"></i>
-			<i className="fi fi-brands-facebook inline-block text-2xl text-red-300 hover:animate-bounce hover:cursor-pointer hover:text-[#ff014f]"></i>
-			<i className="fi fi-brands-twitter-alt inline-block text-2xl text-red-300 hover:animate-bounce hover:cursor-pointer hover:text-[#ff014f]"></i>
+			<Link href="https://github.com/prantiranibanda">
+				<i className="fi fi-brands-github inline-block text-2xl text-red-300 hover:animate-bounce hover:cursor-pointer hover:text-[#ff014f]"></i>
+			</Link>
+			<Link href="https://www.linkedin.com/in/prantiranibanda/">
+				<i className="fi fi-brands-linkedin inline-block text-2xl text-red-300 hover:animate-bounce hover:cursor-pointer hover:text-[#ff014f]"></i>
+			</Link>
+			<Link href="https://www.instagram.com/prantiranibanda">
+				<i className="fi fi-brands-instagram inline-block text-2xl text-red-300 hover:animate-bounce hover:cursor-pointer hover:text-[#ff014f]"></i>
+			</Link>
+			<Link href="https://www.facebook.com/prantiranibanda">
+				<i className="fi fi-brands-facebook inline-block text-2xl text-red-300 hover:animate-bounce hover:cursor-pointer hover:text-[#ff014f]"></i>
+			</Link>
+			<Link href="https://x.com/prantiranibanda">
+				<i className="fi fi-brands-twitter-alt inline-block text-2xl text-red-300 hover:animate-bounce hover:cursor-pointer hover:text-[#ff014f]"></i>
+			</Link>
 		</div>
 	);
 };
