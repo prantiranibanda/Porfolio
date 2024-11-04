38e840c312bea98cb44112861acdaaeb3106c5c0
pranti <prantibanda@gmail.com>
2024-11-04 17:26:01 +0530

    Open link in new tab

--- src/app/components/ContactIcon.jsx
+++ src/app/components/ContactIcon.jsx
@@ -4,19 +4,19 @@ import React from "react";
 const ContactIcon = () => {
 	return (
 		<div className="space-x-8 pt-8 text-white">
-			<Link href="https://github.com/prantiranibanda">
+			<Link href="https://github.com/prantiranibanda" target="_blank"> 
 				<i className="fi fi-brands-github inline-block text-2xl text-red-300 hover:animate-bounce hover:cursor-pointer hover:text-[#ff014f]"></i>
 			</Link>
 			<Link href="https://www.linkedin.com/in/prantiranibanda/">
 				<i className="fi fi-brands-linkedin inline-block text-2xl text-red-300 hover:animate-bounce hover:cursor-pointer hover:text-[#ff014f]"></i>
 			</Link>
-			<Link href="https://www.instagram.com/prantiranibanda">
+			<Link href="https://www.instagram.com/prantiranibanda" target="_blank">
 				<i className="fi fi-brands-instagram inline-block text-2xl text-red-300 hover:animate-bounce hover:cursor-pointer hover:text-[#ff014f]"></i>
 			</Link>
-			<Link href="https://www.facebook.com/prantiranibanda">
+			<Link href="https://www.facebook.com/prantiranibanda" target="_blank">
 				<i className="fi fi-brands-facebook inline-block text-2xl text-red-300 hover:animate-bounce hover:cursor-pointer hover:text-[#ff014f]"></i>
 			</Link>
-			<Link href="https://x.com/prantiranibanda">
+			<Link href="https://x.com/prantiranibanda" target="_blank">
 				<i className="fi fi-brands-twitter-alt inline-block text-2xl text-red-300 hover:animate-bounce hover:cursor-pointer hover:text-[#ff014f]"></i>
 			</Link>
 		</div>
